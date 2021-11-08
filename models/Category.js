caef2c4136759e3003f4fc49b97aa43107f7244b
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2021-11-07 18:25:52 -0600

    finding errors in starting server

--- models/Category.js
+++ models/Category.js
@@ -7,7 +7,7 @@ class Category extends Model {}
 Category.init(
   {
     id: {
-      type: Sequelize.INTEGER,
+      type: sequelize.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
--- models/index.js
+++ models/index.js
@@ -1,20 +1,18 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
-
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-Product.belongsTo (Category, {
-  foreignKey: 'category_id'
-  onDelete: cascade
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  // onDelete: cascade
 });
-
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
@@ -22,17 +20,14 @@ Category.hasMany(Product, {
 //product can have multiple tags and tags can belong to multiple products
 Product.belongsToMany(Tags, {
   through: ProductTag,
-  foreignKey: 'product_id'
+  foreignKey: "product_id",
 });
 
-
-
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product,{
-  through: ProductTag,  
-  foreignKey: 'tag_id'
-})
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
